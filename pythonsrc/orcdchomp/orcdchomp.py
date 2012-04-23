e58382185be6d5bab06f910b527a95a0af2fbdfc
Chris Dellin <cdellin@cs.cmu.edu>
2012-04-23 20:34:01 +0000

    Fixing some syntax errors

--- pythonsrc/orcdchomp/orcdchomp.py
+++ pythonsrc/orcdchomp/orcdchomp.py
@@ -3,12 +3,12 @@ import openravepy
 def runchomp(mod, robot=None, adofgoal=None, n_iter=None, lambda_=None):
    cmd = 'runchomp'
    if robot is not None:
-      if hasattr(robot,'GetName')
+      if hasattr(robot,'GetName'):
          cmd += ' robot %s' % robot.GetName()
       else:
          cmd += ' robot %s' % robot
    if adofgoal is not None:
-      cmd += ' adofgoal %d %s' % (len(adofgoal), ' '.join([str(v) for v in goal_ik]))
+      cmd += ' adofgoal %d %s' % (len(adofgoal), ' '.join([str(v) for v in adofgoal]))
    if n_iter is not None:
       cmd += ' n_iter %d' % n_iter
    if lambda_ is not None:
