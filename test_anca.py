99951ffa852f3b4dcc78ae78022fd6af50684ede
Chris Dellin <cdellin@cs.cmu.edu>
2012-05-11 22:56:45 +0000

    Adding a description, and a test script (which should really go in a new package!)

--- test_anca.py
+++ test_anca.py
@@ -10,13 +10,15 @@ import libprrave.kin
 import libprrave.rave
 import libprrave.tsr
 
+import cbirrt2.cbirrt_problem as cprob
+
 # Start openrave
 openravepy.RaveInitialize(True, level=openravepy.DebugLevel.Info)
 e = openravepy.Environment()
-e.SetViewer('qtcoin')
 
 # Load the environment
 e.Load('environments/intelkitchen_robotized_herb2.env.xml')
+e.SetViewer('qtcoin')
 
 # Set up the robot, ik, active manip, active dofs
 with e:
@@ -27,8 +29,11 @@ with e:
        [-1., 0., 0., -1.3748 ],
        [ 0., 0., 1.,  0.     ],
        [ 0., 0., 0.,  1.     ]]))
+   #r.SetDOFValues(numpy.array(
+   #   [  5.3, -1.8, -0.8,  2.4, -3.2,  0.0,  0.0952,  0.0, 0.0, 0.0, 0.0,
+   #      0.9, -1.9,  0.7,  2.4, -1.6,  0.0,  0.8,     1.6, 1.6, 1.6, 0.0  ]))
    r.SetDOFValues(numpy.array(
-      [  5.3, -1.8, -0.8,  2.4, -3.2,  0.0,  0.0952,  0.0, 0.0, 0.0, 0.0,
+      [  5.3, -1.5,  2.9,  2.4, -3.2,  0.0,  0.0952,  0.0, 0.0, 0.0, 0.0,
          0.9, -1.9,  0.7,  2.4, -1.6,  0.0,  0.8,     1.6, 1.6, 1.6, 0.0  ]))
    r.SetActiveManipulator('right_wam')
    m = r.GetActiveManipulator()
@@ -44,10 +49,15 @@ e.GetKinBody('fuze_bottle').SetTransform(numpy.array(
 
 # Load the box
 e.Load('objects/misc/bigbox.kinbody.xml')
+#e.GetKinBody('bigbox').SetTransform(numpy.array(
+#   [[ 1., 0., 0., -0.5188 ],
+#    [ 0., 1., 0., -2.0258 ],
+#    [ 0., 0., 1.,  0.6005 ],
+#    [ 0., 0., 0.,  1.     ]]))
 e.GetKinBody('bigbox').SetTransform(numpy.array(
-   [[ 1., 0., 0., -0.5188 ],
-    [ 0., 1., 0., -2.0258 ],
-    [ 0., 0., 1.,  0.6005 ],
+   [[ 1., 0., 0., -0.4188 ],
+    [ 0., 1., 0., -1.4258 ],
+    [ 0., 0., 1.,  1.7005 ],
     [ 0., 0., 0.,  1.     ]]))
 
 # Load the rice pilaf
@@ -71,6 +81,7 @@ q_goal = [3.30768, -0.631765, -0.113948, 2.15259, -3.59911, 1.5041, 1.51]
 
 # Create the module
 libprrave.rave.load_module(e, 'orcdchomp', 'blah_load_string')
+libprrave.rave.load_module(e, 'cbirrt', r.GetName())
 
 raw_input('Press [Enter] to view spheres ...')
 libprrave.rave.get_module(e,'orcdchomp').SendCommand(
@@ -91,7 +102,7 @@ r.Enable(True)
 try:
    raw_input('Press [Enter] run chomp ...')
    t_data = libprrave.rave.get_module(e,'orcdchomp').SendCommand(
-      'runchomp robot BarrettWAM adofgoal 7 %s' % (' '.join([str(v) for v in q_goal])))
+      'runchomp robot BarrettWAM adofgoal 7 %s lambda 50.0 n_iter 50' % (' '.join([str(v) for v in q_goal])))
    t = openravepy.RaveCreateTrajectory(e,'').deserialize(t_data)
 except RuntimeError as ex:
    print ex
