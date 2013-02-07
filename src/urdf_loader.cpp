0c196dda4b1120fe8c55a8e6f93aa0d0f2f8a609
Prasanna Velagapudi <pkv@cs.cmu.edu>
2013-02-07 22:48:06 +0000

    Replaced programmatic loading with xml loading because OpenRAVE 0.8.0 only supports that, I think.

--- src/urdf_loader.cpp
+++ src/urdf_loader.cpp
@@ -8,6 +8,7 @@
 
 #include "urdf_loader.h"
 
+#include <tinyxml.h>
 #include <urdf/model.h>
 #include <boost/foreach.hpp>
 #include <boost/make_shared.hpp>
@@ -75,57 +76,42 @@ namespace urdf_loader
       throw OpenRAVE::openrave_exception("Failed to open URDF file!");
     }
 
+    // Create new XML document internally
+    //
+    // NOTE: Some of you may ask, "Why are you making an XML string in here? You
+    // have already parsed the URDF."  Well children, OpenRAVE 0.8 and earlier
+    // seem to have no other mechanism for creating kinbodies with joints and links.
+    //
+    // So, I am forced to do this.  In OpenRAVE 0.9 and above, this code should
+    // probably be replaced with programmatic construction.
+    TiXmlDocument robotXml;
+    robotXml.LinkEndChild(new TiXmlDeclaration("1.0", "", ""));
+    
     // Populate vector of links
-    std::vector<OpenRAVE::KinBody::LinkInfoConstPtr> link_infos;
-
     std::string link_name; 
     boost::shared_ptr<urdf::Link> link_ptr;
     BOOST_FOREACH(boost::tie(link_name, link_ptr), model.links_) {
-
-      // Create a new link and set basic properties
-      OpenRAVE::KinBody::LinkInfoPtr link_info = boost::make_shared<OpenRAVE::KinBody::LinkInfo>();
-      link_info->_name = link_name;
-
-      // Mass parameters
-      urdf::InertialPtr inertial = link_ptr->inertial;
-      link_info->_tMassFrame = URDFPoseToRaveTransform(inertial->origin);
-      link_info->_mass = inertial->mass;
-      link_info->_vinertiamoments = OpenRAVE::Vector(inertial->xx, inertial->yy, inertial->zz);
-
-      // Add geometries for model
-      // TODO: fill me in!
-      
-      link_infos.push_back(link_info);
+      // TODO: fill in links
     }
 
     // Populate vector of joints
-    std::vector<OpenRAVE::KinBody::JointInfoConstPtr> joint_infos;
-
     std::string joint_name; 
     boost::shared_ptr<urdf::Joint> joint_ptr;
     BOOST_FOREACH(boost::tie(joint_name, joint_ptr), model.joints_) {
-
-      // Create a new joint and set basic properties
-      OpenRAVE::KinBody::JointInfoPtr joint_info = boost::make_shared<OpenRAVE::KinBody::JointInfo>();
-      joint_info->_name = joint_name;
-      joint_info->_type = URDFJointTypeToRaveJointType(joint_ptr->type);
-      
-      // Set parent and child links
-      joint_info->_linkname0 = joint_ptr->parent_link_name;
-      joint_info->_linkname1 = joint_ptr->child_link_name;
-
-      // TODO: Fill in everything else!
-
-      joint_infos.push_back(joint_info);
+      // TODO: fill in joints
     }
 
-    // Construct robot from URDF link and joint information
-    OpenRAVE::RobotBasePtr robot = OpenRAVE::RaveCreateRobot(_env, model.getName());
-    robot->Init(link_infos, joint_infos);
-    _env->Add(robot);
+    // Write out the XML document to a string interface
+    // (e.g. http://www.grinninglizard.com/tinyxmldocs/classTiXmlPrinter.html)
+    TiXmlPrinter robotPrinter;
+    robotPrinter.SetStreamPrinting();
+    robotXml.Accept(&robotPrinter);
+    
+    // Load the robot in OpenRAVE from the in-memory XML string
+    _env->LoadData(robotPrinter.CStr());
 
     // Return reference to created object
-    // TODO: return object somehow
+    // TODO: return object somehow?
     return true;
   }
 
