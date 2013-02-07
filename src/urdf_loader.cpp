6a271c84bf89bf900d2a276fb37ddc3386e50200
Prasanna Velagapudi <pkv@cs.cmu.edu>
2013-02-07 03:20:58 +0000

    Converted loader to use URDF stuff that comes with ROS.

--- src/urdf_loader.cpp
+++ src/urdf_loader.cpp
@@ -7,6 +7,7 @@
 /* (C) Copyright 2013 Carnegie Mellon University */
 
 #include "urdf_loader.h"
+#include <urdf/model.h>
 
 /** Boilerplate plugin definition for OpenRAVE */
 OpenRAVE::InterfaceBasePtr CreateInterfaceValidated(OpenRAVE::InterfaceType type, const std::string& interfacename, std::istream& sinput, OpenRAVE::EnvironmentBasePtr env)
@@ -26,16 +27,24 @@ namespace urdf_loader
 {
 
   /** Opens a URDF file and returns a robot in OpenRAVE */
-  bool URDFLoader::load(std::ostream& sout, std::istream& sin)
+  bool URDFLoader::load(std::ostream& soutput, std::istream& sinput)
   {
     // Get filename from input arguments
+    std::string urdf_filename;
+    sinput >> urdf_filename;
+    
+    // Parse file via URDF reader
+    urdf::Model model;
+    if (!model.initFile(urdf_filename)) {
+      RAVELOG_ERROR("URDFLoader : Unable to open URDF file [%s].\n", urdf_filename.c_str());
+      throw OpenRAVE::openrave_exception("Failed to open URDF file!");
+    }
 
-    // Parse file via XML
-
-    // Recursively construct robot from XML
+    // Recursively construct robot from URDF
+    
 
     // Return reference to created object
-
+    // TODO: return object somehow
     return true;
   }
 
