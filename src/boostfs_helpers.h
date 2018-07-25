31b3b2f1efd4a504c16df83910105643c2af031a
Rafael Papallas <rpapallas@gmail.com>
2018-07-25 11:09:57 +0100

    Added support for Boost 1.60+

--- src/boostfs_helpers.h
+++ src/boostfs_helpers.h
@@ -17,36 +17,43 @@ namespace boost
   namespace filesystem3
 #endif
   {
-    template < >
-    path& path::append< typename path::iterator >( typename path::iterator begin, typename path::iterator end, const codecvt_type& cvt)
-    { 
-      for( ; begin != end ; ++begin )
-        *this /= *begin;
-      return *this;
-    }
+    #if BOOST_VERSION > 106000 
+        // From Boost 1.60+ there is a built-in function for this (boost::filesystem::relative).
+        boost::filesystem::path make_relative(boost::filesystem::path a_From, boost::filesystem::path a_To)
+        {
+            return boost::filesystem::relative(a_From, a_To);
+        }
+    #else
+        template < >
+        path& path::append< typename path::iterator >( typename path::iterator begin, typename path::iterator end, const codecvt_type& cvt)
+        { 
+        for( ; begin != end ; ++begin )
+            *this /= *begin;
+        return *this;
+        }
     
-    // Return path when appended to a_From will resolve to same as a_To
-    boost::filesystem::path make_relative( boost::filesystem::path a_From, boost::filesystem::path a_To )
-    {
-      a_From = boost::filesystem::absolute( a_From ); a_To = boost::filesystem::absolute( a_To );
-      boost::filesystem::path ret;
-      boost::filesystem::path::const_iterator itrFrom( a_From.begin() ), itrTo( a_To.begin() );
-      
-      // Find common base
-      for( boost::filesystem::path::const_iterator toEnd( a_To.end() ), fromEnd( a_From.end() ) ; 
-           itrFrom != fromEnd && itrTo != toEnd && *itrFrom == *itrTo; ++itrFrom, ++itrTo );
-      
-      // Navigate backwards in directory to reach previously found base
-      for( boost::filesystem::path::const_iterator fromEnd( a_From.end() ); itrFrom != fromEnd; ++itrFrom ) {
-        if( (*itrFrom) != "." )
-          ret /= "..";
-      }
-      
-      // Now navigate down the directory branch
-      ret.append( itrTo, a_To.end() );
-      return ret;
-    }
+        // Return path when appended to a_From will resolve to same as a_To
+        boost::filesystem::path make_relative( boost::filesystem::path a_From, boost::filesystem::path a_To )
+        {
+        a_From = boost::filesystem::absolute( a_From ); a_To = boost::filesystem::absolute( a_To );
+        boost::filesystem::path ret;
+        boost::filesystem::path::const_iterator itrFrom( a_From.begin() ), itrTo( a_To.begin() );
+        
+        // Find common base
+        for( boost::filesystem::path::const_iterator toEnd( a_To.end() ), fromEnd( a_From.end() ) ; 
+            itrFrom != fromEnd && itrTo != toEnd && *itrFrom == *itrTo; ++itrFrom, ++itrTo );
+        
+        // Navigate backwards in directory to reach previously found base
+        for( boost::filesystem::path::const_iterator fromEnd( a_From.end() ); itrFrom != fromEnd; ++itrFrom ) {
+            if( (*itrFrom) != "." )
+            ret /= "..";
+        }
+        
+        // Now navigate down the directory branch
+        ret.append( itrTo, a_To.end() );
+        return ret;
+        }
+    #endif
   } 
 }
-
 
